0e1dc5a077283c5b7964db05ae0c4edab876359b
Sebastian Laca <seba.laca@gmail.com>
2022-07-11 18:53:58 +0200

    Done

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Section1 from './components/Section1';
+import Section2 from './components/Section2';
 
 function App() {
   return (
     <div className="App">
       <Section1 />
+      <Section2 />
     </div>
   );
 }
